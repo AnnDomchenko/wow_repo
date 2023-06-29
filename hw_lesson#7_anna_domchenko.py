7972e4b450739e03136f0f4a37dbd5e9f50d63ff
AnnDomchenko <andomchenko@gmail.com>
2023-06-29 14:49:25 +0300

    фікс підзадачі 2 по ексепшенам по зауваженню Артема 2

--- hw_lesson#7_anna_domchenko.py
+++ hw_lesson#7_anna_domchenko.py
@@ -5,15 +5,15 @@
 """
 
 
-# def taking_some_input():
-#     try:
-#         var = int(input("Write some integer here: "))
-#         print(var, " is your input")
-#     except ValueError:
-#         print("Entered not valid data")
-#
-#
-# taking_some_input()
+def taking_some_input():
+    try:
+        var = int(input("Write some integer here: "))
+        print(var, " is your input")
+    except ValueError:
+        print("Entered not valid data")
+
+
+taking_some_input()
 
 
 """
@@ -41,39 +41,39 @@ concatenate_or_sum("text", 8)
 """
 
 
-# def int_acceptor():
-#     while True:
-#         try:
-#             value=int(input("Enter: "))
-#             print("thanks and bye")
-#             return value
-#         except ValueError:
-#             print("enter sth else")
-#
-# int_acceptor()
+def int_acceptor():
+    while True:
+        try:
+            value=int(input("Enter: "))
+            print("thanks and bye")
+            return value
+        except ValueError:
+            print("enter sth else")
+
+int_acceptor()
 
 """
 Створити власне виключення. Наприклад OnlyEvenError. Створити функцію check_digit(), яка приймає число.
   Якщо число не парне, то породжувати це своє виключення, якщо парне, то повертати його (return)
 """
 
-# class OnlyEvenError(Exception):
-#     pass
-#
-#
-# def check_digit(a):
-#     if a%2!=0:
-#         raise OnlyEvenError
-#     else:
-#         print("Fine, ", a)
-#
-# try:
-#     a=int(input("enter: "))
-#     check_digit(a)
-# except OnlyEvenError:
-#     print("OnlyEven error successfully raised")
-# except ValueError:
-#     print("You wrote sth wrong at all")
+class OnlyEvenError(Exception):
+    pass
+
+
+def check_digit(a):
+    if a%2!=0:
+        raise OnlyEvenError
+    else:
+        print("Fine, ", a)
+
+try:
+    a=int(input("enter: "))
+    check_digit(a)
+except OnlyEvenError:
+    print("OnlyEven error successfully raised")
+except ValueError:
+    print("You wrote sth wrong at all")
 
 """
 5.  Створити функцію, що буде приймати число як аргумент і викликАти в тілі функцію check_digit, в яку передавати це число.
@@ -84,22 +84,22 @@ concatenate_or_sum("text", 8)
       Використовувати try-except-else-finally
 """
 
-# def some_func(a):
-#     try:
-#         check_digit(a)
-#     except OnlyEvenError:
-#         a += 1
-#         print(a)
-#     except TypeError:
-#         print("bad input!")
-#     else:
-#         a *= 2
-#         print(a)
-#     finally:
-#         print("Я все одно завжди щось друкую")
-#
-#
-# some_func(6)
-# some_func(3)
-# some_func(4.5)
-# some_func("fgh")
+def some_func(a):
+    try:
+        check_digit(a)
+    except OnlyEvenError:
+        a += 1
+        print(a)
+    except TypeError:
+        print("bad input!")
+    else:
+        a *= 2
+        print(a)
+    finally:
+        print("Я все одно завжди щось друкую")
+
+
+some_func(6)
+some_func(3)
+some_func(4.5)
+some_func("fgh")
